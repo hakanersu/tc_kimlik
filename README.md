24247da65f7e9274638fca3341729b3e69b99b5f
Hakan ERSU <hakanersu@gmail.com>
2020-12-07 01:10:31 +0300

    Generator added

--- README.md
+++ README.md
@@ -2,8 +2,20 @@
 
 Tc kimlik is a validator and utility tools for Turkish identification numbers.
 
+### Validating
+
 ```rust
-tc_kimlik::validate("40243834390");
+let  tc = String::from("40243834390");
+tc_kimlik::validate(&tc) // returns true
+```
+
+### Generate valid TC Kimlik
+
+```rust
+extern crate tc_kimlik;
+fn main() {
+    println!("New tc kimlik: {}", tc_kimlik::generate());
+}
 ```
 
 
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+use rand::Rng;
+
 pub fn validate(tc: &String) -> bool {
     let parts = number_to_vec(&tc);
     
@@ -21,6 +23,18 @@ pub fn validate(tc: &String) -> bool {
     }
 
     true
+}
+pub fn generate() -> String {
+    let mut rng = rand::thread_rng();
+    let random: u32 = rng.gen_range(100000000, 999999999);
+    let parts = number_to_vec(&random.to_string());
+    let even: u32 = parts.iter().take(9).step_by(2).sum();
+    let odd: u32 = parts[1..9].iter().step_by(2).sum();
+    let ten: u32 = ( even * 7 - odd) % 10;
+    let new_number = format!("{}{}", random, ten);
+    let new_parts = number_to_vec(&new_number);
+    let eleven: u32 = new_parts[0..10].iter().sum::<u32>() % 10;
+    return format!("{}{}{}", random, ten, eleven)
 }
 
 fn number_to_vec(n: &String) -> Vec<u32> {
